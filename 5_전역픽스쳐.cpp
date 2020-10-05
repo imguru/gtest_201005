60afd3a5493947913e98fbc8b7a472e7fbedc871
Chansik Yun <chansigi@gmail.com>
2020-10-05 23:48:46 +0000

    .

--- 5_전역픽스쳐.cpp
+++ 5_전역픽스쳐.cpp
@@ -5,9 +5,9 @@
 
 #include <gtest/gtest.h>
 
-TEST(SampleTest, Foo) {}
-TEST(SampleTest, Goo) {}
-TEST(SampleTest, Hoo) {}
+TEST(SampleTest1, Foo) {}
+TEST(SampleTest2, Goo) {}
+TEST(SampleTest3, Hoo) {}
 
 // Google Test Framework에 등록되는 객체는 반드시 new를 통해 생성되어야 합니다.
 class MyTestEnvironment : public testing::Environment {
@@ -27,6 +27,31 @@ public:
 // 1. 전역 변수: main을 직접 제공하지 않을 때
 //  전역 변수는 프로그램(main)이 시작되기 전에, 초기화 됩니다.
 //  전역 변수의 초기화 루틴을 이용해서, 함수를 호출합니다.
-testing::Environment* env = testing::AddGlobalTestEnvironment(new MyTestEnvironment);
+
+// testing::Environment* env = testing::AddGlobalTestEnvironment(new MyTestEnvironment);
+// 위 처럼 사용하는 것은 권장되지 않습니다.
+// 전역 픽스쳐를 설정해야 한다면, main을 직접 제공하는 것이 좋습니다.
 
 // 2. main 함수: main을 직접 제공할 때
+int main(int argc, char** argv) {
+	testing::InitGoogleTest(&argc, argv);
+
+	AddGlobalTestEnvironment(new MyTestEnvironment);
+
+	return RUN_ALL_TESTS();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
