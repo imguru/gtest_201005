542b687c9dd227cbf2b0c096339db911559ed8d7
Chansik Yun <chansigi@gmail.com>
2020-10-07 01:26:37 +0000

    .

--- 16_Mocking.cpp
+++ 16_Mocking.cpp
@@ -29,12 +29,18 @@ struct MP3 {
 // - 모의객체가 제대로 생성되었는지 확인하기 위해서는 생성을 해봐야 합니다.
 // - 템플릿의 인자가 2개 이상인 경우, 쉼표(,)로 인해서, 매크로 함수의 인자가 1개 더 전달되는 문제가 있습니다.
 //   => 괄호로 한번 더 묶어주어야 합니다.
+//   => 템플릿 인자가 2개인 타입에 대한 엘리어싱(별명)을 지정하면 편리하게 사용할 수 있습니다.
 
 class MockMP3 : public MP3 {
 public:
-	MOCK_METHOD(bool, CheckMap, ((std::map<int, double>) a, bool b), (override));
+	using MapIntDouble = std::map<int, double>;
+	using BoolAndInt = std::pair<bool, int>;
+	MOCK_METHOD(bool, CheckMap, (MapIntDouble a, bool b), (override));
+	MOCK_METHOD(BoolAndInt, GetPair, (), (override, const));
 
-	MOCK_METHOD((std::pair<bool, int>), GetPair, (), (override, const));
+	// MOCK_METHOD(bool, CheckMap, ((std::map<int, double>) a, bool b), (override));
+	// MOCK_METHOD((std::pair<bool, int>), GetPair, (), (override, const));
+
 	MOCK_METHOD(std::vector<int>, GetVector, (), (override, const));
 
 	MOCK_METHOD(void, Play, (), (override));
